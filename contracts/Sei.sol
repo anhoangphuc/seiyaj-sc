0e7aee22970870a7de906b7af728781ac0b84766
phucta <phuc.ta@sotatek.com>
2024-04-07 21:04:51 +0700

    Add test for sendingMultipleRecipient

--- contracts/Sei.sol
+++ contracts/Sei.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Seiyaj is ERC20, ERC20Capped {
     event WhitelistSet(address indexed user, uint256 amount);
+    event Faucet(address indexed user, uint256 amount);
     // Admin address, used to add whitelist users
     address public admin;
     // Mapping of whitelisted addresses to their maximum allowed balance
@@ -19,6 +20,55 @@ contract Seiyaj is ERC20, ERC20Capped {
         admin = msg.sender;
     }
 
+    //******************** USER FUNCITON *********************/
+
+    /**
+     @notice User call faucet to mint allowed amount of token
+     @param amount Amount of token to mint
+     */
+    function faucet(uint256 amount) external {
+        require(whitelist[msg.sender] >= amount, "Exceed");
+        whitelist[msg.sender] -= amount;
+        _mint(msg.sender, amount);
+        emit Faucet(msg.sender, amount);
+    }
+
+    /**
+     @notice User call to send token to multiple recipient
+     @param users Array of recipient address
+     @param amounts Array of amount to send to each recipient
+     @dev Notice the gas fee
+     */
+    function sendMultipleRecipient(
+        address[] calldata users,
+        uint256[] calldata amounts
+    ) external {
+        require(users.length == amounts.length, "Invalid length");
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < users.length; i++) {
+            totalAmount += amounts[i];
+        }
+        // We early revert here to save the gas fee
+        require(totalAmount <= balanceOf(msg.sender), "Insufficient balance");
+
+        for (uint256 i = 0; i < users.length; i++) {
+            _transfer(msg.sender, users[i], amounts[i]);
+        }
+    }
+
+    //******************** ADMIN FUNCITON *********************/
+    /**
+        @notice Set whitelist for the user with increased amount
+        @param user Address of the user
+        @param amount Increased amount
+        @dev We increase the amount, instead of setting the amount, so that we can easily add more amount to the user.
+        We combine this function with check on backend to prevent spam users
+     */
+    function setWhitelist(address user, uint256 amount) external onlyAdmin {
+        whitelist[user] += amount;
+        emit WhitelistSet(user, whitelist[user]);
+    }
+
     /**
      @notice Admin transfer ownership
      @param newAdmin New admin address
@@ -26,6 +76,8 @@ contract Seiyaj is ERC20, ERC20Capped {
     function transferAdminOwnership(address newAdmin) external onlyAdmin {
         admin = newAdmin;
     }
+
+    //******************** INTERNAL FUNCTION *********************/
 
     function _update(
         address from,
@@ -35,16 +87,7 @@ contract Seiyaj is ERC20, ERC20Capped {
         super._update(from, to, value);
     }
 
-    /**
-        @notice Set whitelist for the user with increased amount
-        @param user Address of the user
-        @param amount Increased amount
-        @dev We increase the amount, instead of setting the amount, so that we can easily add more amount to the user
-     */
-    function setWhitelist(address user, uint256 amount) external onlyAdmin {
-        whitelist[user] += amount;
-        emit WhitelistSet(user, whitelist[user]);
-    }
+    //******************** MODIFIER *********************/
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only Admin");
--- test/Sei.ts
+++ test/Sei.ts
@@ -5,8 +5,9 @@ import {
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
-import { WeiPerEther } from "ethers";
+import { WeiPerEther, ZeroAddress } from "ethers";
 import { Seiyaj } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("Sei", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -57,6 +58,87 @@ describe("Sei", function () {
         .to.be.emit(seiyaj, "WhitelistSet")
         .withArgs(user1.address, amount + amount2);
       expect(await seiyaj.whitelist(user1.address)).to.be.eq(amount + amount2);
+    });
+  });
+
+  describe("User function", function () {
+    let amount: bigint;
+    let seiyaj: Seiyaj;
+    let user1: HardhatEthersSigner;
+    beforeEach(async () => {
+      amount = BigInt(10) * WeiPerEther;
+      ({ seiyaj, user1 } = await loadFixture(deploy));
+      await seiyaj.setWhitelist(user1.address, amount);
+    });
+
+    it("Should faucet success", async function () {
+      await expect(seiyaj.connect(user1).faucet(amount))
+        .to.be.emit(seiyaj, "Faucet")
+        .withArgs(user1.address, amount)
+        .to.be.emit(seiyaj, "Transfer")
+        .withArgs(ZeroAddress, user1.address, amount);
+    });
+
+    it("Should faucet failure exceed", async function () {
+      await seiyaj.connect(user1).faucet(amount / BigInt(2));
+      await expect(
+        seiyaj.connect(user1).faucet(amount / BigInt(2) + BigInt(1)),
+      ).to.be.rejectedWith("Exceed");
+    });
+
+    it("Should send multiple users success", async function () {
+      await seiyaj.connect(user1).faucet(amount);
+      const [user2, user3] = [
+        ethers.Wallet.createRandom(),
+        ethers.Wallet.createRandom(),
+      ];
+      const amount2 = (amount * BigInt(4)) / BigInt(6);
+      const amount3 = amount - amount2;
+
+      await expect(
+        seiyaj
+          .connect(user1)
+          .sendMultipleRecipient(
+            [user2.address, user3.address],
+            [amount2, amount3],
+          ),
+      )
+        .to.be.emit(seiyaj, "Transfer")
+        .withArgs(user1.address, user2.address, amount2)
+        .to.be.emit(seiyaj, "Transfer")
+        .withArgs(user1.address, user3.address, amount3);
+    });
+
+    it("Should send multiple users failure Invalid length ", async function () {
+      const [user2, user3] = [
+        ethers.Wallet.createRandom(),
+        ethers.Wallet.createRandom(),
+      ];
+
+      await expect(
+        seiyaj
+          .connect(user1)
+          .sendMultipleRecipient([user2.address, user3.address], [BigInt(1)]),
+      ).to.be.revertedWith("Invalid length");
+    });
+
+    it("Should send multiple users failure Insufficient balance", async function () {
+      const [user2, user3] = [
+        ethers.Wallet.createRandom(),
+        ethers.Wallet.createRandom(),
+      ];
+
+      const amount2 = (amount * BigInt(4)) / BigInt(6);
+      const amount3 = amount - amount2;
+
+      await expect(
+        seiyaj
+          .connect(user1)
+          .sendMultipleRecipient(
+            [user2.address, user3.address],
+            [amount2, amount3 + BigInt(1)],
+          ),
+      ).to.be.revertedWith("Insufficient balance");
     });
   });
 });
