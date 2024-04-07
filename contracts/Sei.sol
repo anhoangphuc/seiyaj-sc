6f0271e2b0cabc2b110775964c6fedd12112d253
phucta <phuc.ta@sotatek.com>
2024-04-07 20:01:09 +0700

    Add method setWhitelist

--- contracts/Sei.sol
+++ contracts/Sei.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Seiyaj is ERC20, ERC20Capped {
+    event WhitelistSet(address indexed user, uint256 amount);
     // Admin address, used to add whitelist users
     address public admin;
     // Mapping of whitelisted addresses to their maximum allowed balance
@@ -22,8 +23,7 @@ contract Seiyaj is ERC20, ERC20Capped {
      @notice Admin transfer ownership
      @param newAdmin New admin address
      */
-    function transferAdminOwnership(address newAdmin) external {
-        require(msg.sender == admin, "Only admin can transfer ownership");
+    function transferAdminOwnership(address newAdmin) external onlyAdmin {
         admin = newAdmin;
     }
 
@@ -33,5 +33,21 @@ contract Seiyaj is ERC20, ERC20Capped {
         uint256 value
     ) internal override(ERC20, ERC20Capped) {
         super._update(from, to, value);
+    }
+
+    /**
+        @notice Set whitelist for the user with increased amount
+        @param user Address of the user
+        @param amount Increased amount
+        @dev We increase the amount, instead of setting the amount, so that we can easily add more amount to the user
+     */
+    function setWhitelist(address user, uint256 amount) external onlyAdmin {
+        whitelist[user] += amount;
+        emit WhitelistSet(user, whitelist[user]);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only Admin");
+        _;
     }
 }
--- test/Sei.ts
+++ test/Sei.ts
@@ -13,11 +13,11 @@ describe("Sei", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deploy() {
-    const [admin] = await ethers.getSigners();
+    const [admin, user1] = await ethers.getSigners();
     const SeiContract = await ethers.getContractFactory("Seiyaj");
     const seiyaj = (await SeiContract.deploy()) as Seiyaj;
 
-    return { seiyaj, admin };
+    return { seiyaj, admin, user1 };
   }
 
   describe("Deployment", function () {
@@ -27,6 +27,36 @@ describe("Sei", function () {
       expect(await seiyaj.getAddress()).to.be.properAddress;
       expect(await seiyaj.cap()).to.be.eq(BigInt(1000000000) * WeiPerEther);
       expect(await seiyaj.admin()).to.be.eq(admin.address);
+    });
+  });
+
+  describe("Admin function", function () {
+    it("Should set new admin success", async function () {
+      const { seiyaj, admin, user1 } = await loadFixture(deploy);
+      await seiyaj.connect(admin).transferAdminOwnership(user1.address);
+      expect(await seiyaj.admin()).to.be.eq(user1.address);
+    });
+
+    it("Should set admin failure Only Admin", async function () {
+      const { seiyaj, admin, user1 } = await loadFixture(deploy);
+      await expect(
+        seiyaj.connect(user1).transferAdminOwnership(user1.address),
+      ).to.be.revertedWith("Only Admin");
+    });
+
+    it("Set whitelist success", async function () {
+      const { seiyaj, admin, user1 } = await loadFixture(deploy);
+      const amount = BigInt(5) * WeiPerEther;
+      await expect(seiyaj.connect(admin).setWhitelist(user1.address, amount))
+        .to.be.emit(seiyaj, "WhitelistSet")
+        .withArgs(user1.address, amount);
+      expect(await seiyaj.whitelist(user1.address)).to.be.eq(amount);
+
+      const amount2 = BigInt(10) * WeiPerEther;
+      await expect(seiyaj.connect(admin).setWhitelist(user1.address, amount2))
+        .to.be.emit(seiyaj, "WhitelistSet")
+        .withArgs(user1.address, amount + amount2);
+      expect(await seiyaj.whitelist(user1.address)).to.be.eq(amount + amount2);
     });
   });
 });
